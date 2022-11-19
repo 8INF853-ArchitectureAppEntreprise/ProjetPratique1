0c1716d3f9a5a4071321d7d28b45fb870cb79174
adrien1212 <batman.caubel@laposte.net>
2022-11-19 16:00:39 -0500

    Initial commit

---
+++ src/main/java/uqac/groupe6/prixbanque/PrixbanqueApplication.java
@@ -0,0 +1,21 @@
+package uqac.groupe6.prixbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@RestController
+public class PrixbanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrixbanqueApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+}
